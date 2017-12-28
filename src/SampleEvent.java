f30d26a0c224b5d9ff3df29bf92ea9bccba59c00
크게접은하늘 <ykorn012@gmail.com>
2017-12-28 20:23:44 +0900

    Create SampleEvent.java

---
+++ src/SampleEvent.java
@@ -0,0 +1,28 @@
+package com.isd.esper.esper;
+
+public class SampleEvent {
+	private String item = null;
+	private Double price = null;
+
+	public SampleEvent(String item, Double price) {
+		super();
+		this.item = item;
+		this.price = price;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
